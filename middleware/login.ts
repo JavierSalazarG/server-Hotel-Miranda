ac5089747637539d51c954aedfb03a86dda9ec4a
Javi <javis9555@gm@gmail.com>
2023-12-22 10:18:57 +0100

    finish middleware

--- middleware/login.ts
+++ middleware/login.ts
@@ -3,9 +3,11 @@ import { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
 interface AuthenticatedRequest extends Request {
   perfil?: any;
 }
+
 const KEY_ENV = process.env.JWT_SECRET;
 
 export const loginAuthenticationMiddleware = async (
@@ -14,10 +16,13 @@ export const loginAuthenticationMiddleware = async (
   next: NextFunction
 ) => {
   const auth = req.headers["authorization"];
-  if (!auth)
+
+  if (!auth) {
     return res
       .status(401)
-      .json({ error: true, message: 'la autorización no está en el encabezado' });
+      .json({ error: true, message: 'La autorización no está en el encabezado' });
+  }
+
   const token = auth.split('Bearer ')[1];
 
   if (!token) {
@@ -29,8 +34,12 @@ export const loginAuthenticationMiddleware = async (
         req.perfil = perfil;
         next();
       } catch (err) {
+        console.error('Error al verificar el token:', err);
         return res.status(403).json({ error: 'Error en el token' });
       }
+    } else {
+      console.error('La clave JWT no está configurada correctamente');
+      return res.status(500).json({ error: 'Error interno del servidor' });
     }
   }
 };
