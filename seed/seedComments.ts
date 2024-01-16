0d42e18539af234bbb3e7a0fd53122bd7b6332e4
¨javi <javis9555@gmail.com>
2024-01-16 18:24:04 +0100

    fix modificxado models de comments para sql

--- seed/seedComments.ts
+++ seed/seedComments.ts
@@ -1,25 +1,12 @@
-// import mongoose from "mongoose";
-// import { connectMongo } from "../config/mongo";
-// import { commentsModel } from "../models/comments";
-// import { faker } from "@faker-js/faker";
-// async function createComments() {
-//   try {
-//     await connectMongo();
-//     for (let index = 0; index <= 10; index++) {
-//       const comment = new commentsModel({
-//         name: faker.person.fullName(),
-//         fecha: faker.date.past().toLocaleDateString(),
-//         foto_perfil: faker.image.avatarLegacy(),
-//         archive: false,
-//         comentario: faker.lorem.sentence(),
-//       });
-//       await comment.save();
-//     }
-//   } catch (error) {
-//     console.error(error);
-//   } finally {
-//     mongoose.connection.close();
-//   }
-// }
+import { connectSQL } from "../config/sql";
+import { commentsJoi } from "../models/comments";
+import { faker } from "@faker-js/faker";
+async function createComments() {
+  try {
+    await connectSQL();
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-// createComments();
+createComments();
